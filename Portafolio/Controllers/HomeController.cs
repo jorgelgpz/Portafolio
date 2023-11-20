fda6b00a0327ff3ec1cb6cb6249125248e6aa899
jorgelgpz <jorge.lgpz@gmail.com>
2023-11-20 14:43:09 -0600

    Listado de Proyectos

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -12,10 +12,46 @@ namespace Portafolio.Controllers
         {
             _logger = logger;
         }
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link = "https://amazon.com",
+                    ImagenURL = "/imagenes/amazon.PNG"
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Página de noticias en React",
+                    Link = "https://nytimes.com",
+                    ImagenURL = "/imagenes/nyt.PNG"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://reddit.com",
+                    ImagenURL = "/imagenes/reddit.PNG"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para compra de videojuegos",
+                    Link = "https://steam.com",
+                    ImagenURL = "/imagenes/steam.PNG"
+                },
 
+            };
+        }
         public IActionResult Index()
         {
-            return View();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
         }
 
         public IActionResult Privacy()
