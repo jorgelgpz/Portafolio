50ebb515fa1651deaccbe2af5c5f9250d2fee944
jorgelgpz <jorge.lgpz@gmail.com>
2023-11-21 10:24:29 -0600

    Principio de Responsabilidad Única

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 using System.Diagnostics;
 
 namespace Portafolio.Controllers
@@ -12,44 +13,11 @@ namespace Portafolio.Controllers
         {
             _logger = logger;
         }
-        private List<Proyecto> ObtenerProyectos()
-        {
-            return new List<Proyecto>()
-            {
-                new Proyecto
-                {
-                    Titulo = "Amazon",
-                    Descripcion = "E-Commerce realizado en ASP.NET Core",
-                    Link = "https://amazon.com",
-                    ImagenURL = "/imagenes/amazon.PNG"
-                },
-                new Proyecto
-                {
-                    Titulo = "New York Times",
-                    Descripcion = "Página de noticias en React",
-                    Link = "https://nytimes.com",
-                    ImagenURL = "/imagenes/nyt.PNG"
-                },
-                new Proyecto
-                {
-                    Titulo = "Reddit",
-                    Descripcion = "Red social para compartir en comunidades",
-                    Link = "https://reddit.com",
-                    ImagenURL = "/imagenes/reddit.PNG"
-                },
-                new Proyecto
-                {
-                    Titulo = "Steam",
-                    Descripcion = "Tienda en línea para compra de videojuegos",
-                    Link = "https://steam.com",
-                    ImagenURL = "/imagenes/steam.PNG"
-                },
 
-            };
-        }
         public IActionResult Index()
         {
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var repositorioProyectos = new RepositorioProyectos();
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
         }
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,43 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link = "https://amazon.com",
+                    ImagenURL = "/imagenes/amazon.PNG"Z
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Página de noticias en React",
+                    Link = "https://nytimes.com",
+                    ImagenURL = "/imagenes/nyt.PNG"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://reddit.com",
+                    ImagenURL = "/imagenes/reddit.PNG"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para compra de videojuegos",
+                    Link = "https://steam.com",
+                    ImagenURL = "/imagenes/steam.PNG"
+                },
+
+            };
+        }
+    }
+}
